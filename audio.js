68671d8cbac66ba153f7d17291b88139363252b0
Houvd <longogiuseppe291201@gmail.com>
2023-12-12 18:24:37 +0100

    aggiunta waveform

--- audio.js
+++ audio.js
@@ -1,24 +1,23 @@
 const fileSelector = document.getElementById('file-selector');
 const fileNameElement = document.getElementById('fileName');
-const duration = document.getElementById('duration');
 const playButton = document.getElementById('play');
 const stopButton = document.getElementById('stop');
 let audioCtx;
-let source;  
-let pauseFlag = false;
+let source;
+let wavesurfer;
+
 audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 const gainNode = audioCtx.createGain();
-
 
 fileSelector.addEventListener('change', (event) => {
   const fileList = event.target.files;
   if (fileList.length > 0) {
     const fileType = fileList[0].type;
-    var fileName = fileList[0].name;
+    const fileName = fileList[0].name;
     fileNameElement.textContent = fileName;
 
     if (fileType.startsWith('audio/')) {
-        createBuffer(fileList[0]);
+      createBuffer(fileList[0]);
     } else {
       alert('Seleziona un file audio.');
       fileNameElement.textContent = '';
@@ -29,84 +28,58 @@ fileSelector.addEventListener('change', (event) => {
   }
 });
 
-
-
 function createBuffer(file) {
-  if(source)
-    {
-      source.stop();
-    }
+  if (source) {
+    source.stop();
+  }
   const reader = new FileReader();
   reader.onload = function (event) {
     const audioData = event.target.result;
 
-    //audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
-    audioCtx.decodeAudioData(audioData, function(buffer) {
-     
+    audioCtx.decodeAudioData(audioData, function (buffer) {
       console.log(buffer);
-      
-   
-      sumeffects(buffer);
-   
+      sumeffects(buffer, audioData);
     });
   };
 
   reader.readAsArrayBuffer(file);
-  console.log(reader);
 }
 
-function sumeffects(buffer) {
- 
+function sumeffects(buffer, audioData) {
   source = audioCtx.createBufferSource();
-  
-  //buffer dovrà essere inserito in una funzione chiamata modifyBuffer prima di essere
-  //inserito in source
-  //se inseriamo il valore in secondi e lo moltiplichiamo in sr, otteniamo il campione
-  //dal quale partire
   source.buffer = buffer;
 
-  gainNode.gain.value = 0.5; 
+  gainNode.gain.value = 0.5;
   source.connect(gainNode);
-  gainNode.connect(audioCtx.destination);
   source.loop = true;
-  //reset the flags
+
+  wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: 'rgb(200, 0, 200)',
+    progressColor: 'rgb(100, 0, 100)',
+    backend: 'WebAudio',
+  });
+
+  wavesurfer.loadBlob(new Blob([audioData]));
+
   playButton.disabled = false;
-  stopButton.disabled = true;
-  pauseFlag = false;
-}
+  stopButton.disabled = true;  
 
-
-
-
-playButton.addEventListener("click", playSound, pauseFlag);
-stopButton.addEventListener("click", stopSound, pauseFlag);
-function playSound() {
-  if (source) {
+  playButton.addEventListener('click', function () {
+    wavesurfer.play();
     playButton.disabled = true;
-    if (pauseFlag == true){
-      gainNode.gain.linearRampToValueAtTime(0.5, audioCtx.currentTime+0.05);
-      setTimeout(resumeFlag, 100);
-    } 
-    else{
-      source.start();
-      console.log(source);
-      stopButton.disabled = false;
-    }
-    pauseFlag = false;
-  }
+    stopButton.disabled = false;  
+  });
+
+  stopButton.addEventListener('click', function () {
+    wavesurfer.pause();
+    playButton.disabled = false;
+    stopButton.disabled = true;
+  });
 }
-function stopFlag(){
-  audioCtx.suspend();
-  playButton.disabled = false;
-}
-function resumeFlag(){
-  audioCtx.resume();
-  stopButton.disabled = false;
-}
-function stopSound() {
-  pauseFlag = true;
-  stopButton.disabled = true;
-  gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime+0.05);
-  setTimeout(stopFlag, 100);
-}
+
+
+// Rimossi i codici di play e stop, perchè la libreria di wavesurfer si occupa 
+// in autonomia della gestione di queste features
+
+
